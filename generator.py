bdcc8836aa5c55568fbc8c409ade8081723c6bed
AlexisFebvre <alexis.febvre67@gmail.com>
2024-02-16 14:16:08 +0100

    recup donnees

--- generator.py
+++ generator.py
@@ -3,12 +3,35 @@ import csv
 ## FONCTIONS ##
 # récupère les données d'un ficher
 def recupData(_filename):
+    """
+        récupération des données d'un fichier
+        retourne une liste de liste sous forme de [nom, val1, val2, ...., valn]
+    """
     output = []
     
     file = open(_filename, 'rt')
-    csvReader = csv.reader(file, delimiter=",")
+    csvReader = csv.reader(file, delimiter=";")
 
-    output = csvReader
+    output = [line for line in csvReader]
     return output
 
-print(recupData("donneesMeteo23mars22.csv"))
+datas = recupData("donneesMeteo23mars22.csv")
+
+def recupListe(nom, numero):
+    """
+        arguments:
+            nom -> les données
+            numero -> l'index des données
+        retourne une liste de variable selon l'index
+    """
+    output = []
+    for i in range(1, len(nom[numero])):
+        output.append(int(nom[numero][i]))
+
+    return output
+
+print(recupListe(datas, 0))
+
+
+
+
